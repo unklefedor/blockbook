02c920ac6fbc32610f55de7090e5a9c1dcfe0d73
Алексеев Михаил <unklefedor@gmail.com>
2020-03-02 00:35:10 +0300

    fix

--- server/public.go
+++ server/public.go
@@ -176,8 +176,9 @@ func (s *PublicServer) ConnectFullPublicInterface() {
 	serveMux.HandleFunc(path+"api/sendtx/", s.jsonHandler(s.apiSendTx, apiDefault))
 	serveMux.HandleFunc(path+"api/estimatefee/", s.jsonHandler(s.apiEstimateFee, apiDefault))
 	serveMux.HandleFunc(path+"api/balancehistory/", s.jsonHandler(s.apiBalanceHistory, apiDefault))
+	serveMux.HandleFunc(path+"api/last-blocks/", s.jsonHandler(s.apiBlocks, apiDefault))
 	// v2 format
-	serveMux.HandleFunc(path+"api/v2/blocks/", s.jsonHandler(s.apiBlocks, apiV2))
+	serveMux.HandleFunc(path+"api/v2/last-blocks/", s.jsonHandler(s.apiBlocks, apiV2))
 	serveMux.HandleFunc(path+"api/v2/block-index/", s.jsonHandler(s.apiBlockIndex, apiV2))
 	serveMux.HandleFunc(path+"api/v2/tx-specific/", s.jsonHandler(s.apiTxSpecific, apiV2))
 	serveMux.HandleFunc(path+"api/v2/tx/", s.jsonHandler(s.apiTx, apiV2))
@@ -1098,7 +1099,7 @@ func (s *PublicServer) apiBlocks (r *http.Request, apiVersion int) (interface{},
 		return nil, err
 	}
 
-	return blocks, nil
+	return blocks, err
 }
 
 func (s *PublicServer) apiBlock(r *http.Request, apiVersion int) (interface{}, error) {
